c7a5c20e5bb9977cc4a0ddbd8b9276d69690c575
financefly-ia <financefly.ia@gmail.com>
2025-11-12 14:28:52 -0300

    fix line endings LF and exec

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh start.sh
+web: streamlit run app.py --server.port=$PORT --server.address=0.0.0.0
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "PORT env var from Railway: $PORT"
-PORT=${PORT:-8080}
-
-echo "Starting Streamlit app on Railway..."
-exec streamlit run app.py --server.port=$PORT --server.address=0.0.0.0 --logger.level=debug
